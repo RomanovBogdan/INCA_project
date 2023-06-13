b3fd8c3fef3871f079be4036de612ab782b0fb78
Bogdan <romanovbogdan4@gmail.com>
2023-06-13 16:25:46 +0300

    Update code, so now it collects all texts

--- AppleNewsroom.py
+++ AppleNewsroom.py
@@ -3,7 +3,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-
 class ScrapperWithPageNum:
     def __init__(self, main_body):
         self.main_body = main_body
@@ -33,30 +32,33 @@ class ScrapperWithPageNum:
         return content.find(element, class_=re.compile(f'^{category_element}')).get_text()
 
     @staticmethod
-    def collect_text(content, element, text_element1, text_element2):
+    def collect_text(content, element):
         # div, 'hero-headline', 'pagebody text component'
-        return content.find(element, [text_element1, text_element2]).get_text()
+        return content.find(element).get_text()
 
-    def collect_content(self, link, article_element):
-        page_link = f'{self.main_body}{link}'
-        r = requests.get(page_link)
+    def collect_content(self, link):
+        self.page_link = f'{self.main_body}{link}'
+        r = requests.get(self.page_link)
         soup = BeautifulSoup(r.content, 'html.parser')
-        content = soup.find(article_element)
-
-        return content
+        return soup
 
 
 scrapper = ScrapperWithPageNum('https://www.apple.com/')
 info_list = []
-for i in range(1, 4):
-    soup = scrapper.select_page('/newsroom/archive/?page=', i)
+for page_num in range(1, 209): # 208 is MAX value for page_num
+    soup = scrapper.select_page('/newsroom/archive/?page=', page_num)
     links = scrapper.collect_links(soup, 'div', 'results')
     for link in links:
-        content = scrapper.collect_content(link, 'article')
-        date = scrapper.collect_date(content, 'span', 'category-eyebrow__date')
-        category = scrapper.collect_category(content, 'span', 'category-eyebrow__')
-        text = scrapper.collect_text(content, 'div', 'hero-headline', 'pagebody text component')
+        try:
+            soup = scrapper.collect_content(link)
+            date = scrapper.collect_date(soup, 'span', 'category-eyebrow__date')
+            category = scrapper.collect_category(soup, 'span', 'category-eyebrow__')
 
+            text = scrapper.collect_text(soup, 'article')
+            text_cl = re.sub('\n', ' ', text)
+            text = re.sub(' +', ' ', text_cl)
+        except:
+            date, category, text = None, None, None
         info_list.append({'url': link,
                           'date': date,
                           'category': category,
@@ -64,4 +66,4 @@ for i in range(1, 4):
                           })
 
 test = pd.DataFrame(info_list)
-# test.to_csv('apple_newsroom_text.csv')
+test.to_csv('apple_newsroom_text.csv')
