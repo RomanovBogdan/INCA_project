4a6c76f378406bbb54d0f653372602a0bde2fb2e
Bogdan <romanovbogdan4@gmail.com>
2023-07-25 14:49:20 +0300

    Add function for Factiva text pre-processing

---
+++ Factiva_pipeline/text_pre_processing.py
@@ -0,0 +1,18 @@
+import spacy
+
+# you should pre-install the `en_core_web_sm` model, before loading it
+# python3 -m spacy download en_core_web_sm
+nlp = spacy.load("en_core_web_sm")
+
+def clean_text(text):
+    # Parse the sentence using the loaded 'en_core_web_sm' model object `nlp`
+    file = nlp(text)
+
+    # Extract the lemma for each token and join
+    lemmatized_text = " ".join([token.lemma_ for token in file if token.pos_ in ["NOUN", "VERB", "ADJ"]])
+    return lemmatized_text
+
+text = "INCA, 'Increase corporate political responsibility and accountability', investigates the impact " \
+       "that digital platforms have on European democracies and institutions"
+cleaned_text = clean_text(text)
+print(cleaned_text)
