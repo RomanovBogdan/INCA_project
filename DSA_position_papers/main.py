0ca6e028a6d14dd2387a09a07913433b7f0c6f0f
Bogdan <romanovbogdan4@gmail.com>
2023-08-01 21:02:22 +0300

    Improve the quality of OCR and extraction from text-based files

--- DSA_position_papers/main.py
+++ DSA_position_papers/main.py
@@ -13,8 +13,9 @@ def extract_text_from_pdf(file_path):
     for page_num in range(len(pdf_reader.pages)):
         page_obj = pdf_reader.pages[page_num]
         text += page_obj.extract_text()
+        output_string = ' '.join(text.split())
     pdf_file_obj.close()
-    return text
+    return output_string
 
 
 def get_files_from_folder(folder_name):
@@ -39,40 +40,54 @@ def extract_text_from_image(pdf_path):
     text = ''
     pages = convert_from_path(pdf_path)
     for i, page in enumerate(pages):
-        text_tmp = pytesseract.image_to_string(page)
+        text_tmp = str(pytesseract.image_to_string(page))
         text += text_tmp
-    return text
+        output_string = ' '.join(text.split())
+    return output_string
 
-def collect_text_from_PDF(paths):
+
+def remove_links(text):
+    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+' \
+                  r'|www\.(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    return re.sub(url_pattern, '', text)
+
+
+def remove_separators(text):
+    consecutive_chars_pattern = r'(.)\1{2,}'
+    return re.sub(consecutive_chars_pattern, ' ', text)
+
+
+def max_length(text):
+    length = [len(element) for element in text.split()]
+    length.sort(reverse=True)
+    return length[:5]
+
+
+def collect_text_from_pdf(paths):
     data_list = []
     for i, pdf_path in enumerate(paths):
         print(i)
         try:
             text_pdf = extract_text_from_pdf(pdf_path)
+            method = 'from PDF'
             if len(text_pdf) < 1500:
                 text_pdf = extract_text_from_image(pdf_path)
-            if not text_pdf:
-                raise ValueError("No text extracted from PDF")
-        except:
-            text_pdf = extract_text_from_image(pdf_path)
+                method = 'OCR due to short length'
         finally:
+            cleaned_text = clean_text(text_pdf)
             data_list.append({'link': pdf_path,
-                              'file_name': pdf_path.split('/')[-1],
                               'folder': pdf_path.split('/')[-2],
-                              'text': clean_text(text_pdf)})
+                              'file_name': pdf_path.split('/')[-1],
+                              'text': cleaned_text,
+                              'method': method,
+                              'old_max_length': max_length(cleaned_text),
+                              'new_max_length': max_length(remove_links(cleaned_text))})
     return data_list
 
-def add_spaces_to_text(text):
-    # Add spaces after punctuation marks and before capitalized words
-    text_with_spaces = re.sub(r'([.!?,:;])(\w)', r'\1 \2', text)
-    text_with_spaces = re.sub(r'(\w)([A-Z])', r'\1 \2', text_with_spaces)
-    return text_with_spaces
 
 pdf_paths = get_files_from_folder('Task_2_DSA_position_papers')
-text_list = collect_text_from_PDF(pdf_paths)
+text_list = collect_text_from_pdf(pdf_paths)
 text_df = pd.DataFrame(text_list)
-
 text_df['len'] = [len(text) for text in text_df.text]
-add_spaces_to_text(text_df.text[0])
 
 text_df.to_csv('DSA_position_paper_improved.csv')
