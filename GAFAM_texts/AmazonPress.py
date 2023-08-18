4f0240e6f1f5460d6fdbfae011a440c82de900f2
Bogdan <romanovbogdan4@gmail.com>
2023-08-18 11:53:18 +0300

    Minor changes to the parser

--- GAFAM_texts/AmazonPress.py
+++ GAFAM_texts/AmazonPress.py
@@ -9,12 +9,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+
 def start_driver():
     chrome_driver_path = './chromedriver-mac-arm64/chromedriver'
     service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     return driver
+
 
 def collect_links(driver, existing_links):
     title_elements = driver.find_elements(By.CLASS_NAME, 'PromoCardSearchResults-title')
@@ -25,9 +27,11 @@ def collect_links(driver, existing_links):
         if link_url not in existing_links:
             existing_links.append(link_url)
 
+
 def press_next_page_button(driver):
     next_page = driver.find_element(By.CLASS_NAME, 'SearchResultsModuleResults-nextPage-button')
     next_page.click()
+
 
 def collect_all_links():
     main_body = 'https://press.aboutamazon.com/press-release-archive'
@@ -44,21 +48,25 @@ def collect_all_links():
         i += 1
     return all_links
 
+
 def collect_text(soup):
     entry_content = soup.find('article', class_=re.compile(r"-mainContent$"))
     text_list = [i.get_text() for i in entry_content.find_all('p')]
     return text_list
+
 
 def convert_to_datetime(date_string):
     date_str = date_string[:-1]
     date_obj = datetime.fromisoformat(date_str)
     return date_obj
 
+
 def filter_text(text_list, phrases_to_remove_after):
     for i, text in enumerate(text_list):
         if any(phrase in text for phrase in phrases_to_remove_after):
             return text_list[:i]
     return text_list
+
 
 links = collect_all_links()
 
@@ -70,7 +78,6 @@ phrases_to_remove_after = [
     'About\xa0Amazon'
 ]
 
-link = 'https://press.aboutamazon.com/2020/2/amazon-expands-in-santa-barbara-and-announces-plans-to-create-150-tech-jobs-in-central-california'
 for link in links:
     print(link)
 
